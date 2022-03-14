cee3e98e4dba6cff6473862854b1836e23541ed7
synnemar <synne2006@hotmail.com>
2022-03-14 11:28:41 +0100

    oppdatert testlege

--- IN1010/oblig_4 felles/src/TestLege.java
+++ IN1010/oblig_4 felles/src/TestLege.java
@@ -5,10 +5,13 @@ public class TestLege {
         Lege aese = new Lege("Dr. Æse Bergeland");
         Lege zara = new Lege("Dr. Zara Zulu");
 
-        zara.nyResept(new HvitResept(new Vanedannende("Zoritox", 200, 245, 67), zara, 1008658765, 3));
-        zara.nyResept(new HvitResept(new Vanlig("Paracet", 1000, 199), zara, 612658765, 2));
-        zara.nyResept(new BlaaResept(new Narkotisk("Morfex", 3, 798, 200), zara, 69986, 25));
+        Pasient julie = new Pasient("Julie Haukaas", "010198");
+        Pasient synne = new Pasient("Synne Markmanrud", "260496");
+        Pasient jorgen = new Pasient("Jorgen Osberg", "060598");
 
+        zara.nyResept(new HvitResept(new Vanedannende("Zoritox", 200, 245, 67), zara, julie, 3));
+        zara.nyResept(new HvitResept(new Vanlig("Paracet", 1000, 199), zara, synne, 2));
+        zara.nyResept(new BlaaResept(new Narkotisk("Morfex", 3, 798, 200), zara, jorgen, 25));
 
 
         System.out.println(bob.compareTo(yasmin));
