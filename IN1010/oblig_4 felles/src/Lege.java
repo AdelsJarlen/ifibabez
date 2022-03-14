b3818dc290797e9eaf30396e9bb99e276c1eff8d
coderinthenorth <54352891+coderinthenorth@users.noreply.github.com>
2022-03-14 11:21:32 +0100

    Created a 'Lege' class and a 'TestLege' test file for the class.

--- IN1010/oblig_4 felles/src/Lege.java
+++ IN1010/oblig_4 felles/src/Lege.java
@@ -4,13 +4,14 @@ altsaa vanlige leger. Men hvis legen er spesialist skal det i stedet opprettes
 et objekt av child-klassen Spesialist.
 */
 
-public class Lege {
+public class Lege implements Comparable<Lege>{
 
     /*******************/
     /* KLASSEVARIABLER */
     /*******************/
 
     private String navn;
+    private IndeksertListe<Resept> utskrevneResepter = new IndeksertListe<>();
 
     /****************/
     /* KONSTRUKTOER */
@@ -31,5 +32,29 @@ public class Lege {
     @Override
     public String toString() {
         return "Navn: " + hentNavn();
+    }
+
+    /**
+     * Sammenlikner to strenger basert på hver karakters unicode-verdi. 
+     * Metoden fungerer ikke pålitelig med navn som inneholder spesialkarakterer som 'æ, ø og å'.
+     * @return et heltall med differansen mellom unicode-verdien til Lege 'a' subtrahert 'b'.
+     */
+    @Override
+    public int compareTo(Lege o) {
+        String a = this.navn.toLowerCase();
+        String b = o.navn.toLowerCase();
+
+        int r = a.compareTo(b);
+
+        return r;
+    }
+
+
+    public void nyResept(Resept o){
+        this.utskrevneResepter.leggTil(o);
+    }
+
+    public String hentResepter() {
+        return utskrevneResepter.toString();
     }
 }
