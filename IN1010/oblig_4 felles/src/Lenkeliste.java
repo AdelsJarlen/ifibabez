3e871e0b91568882dc5f522e27bbe841b18156b9
julieaha <89353545+julieaha@users.noreply.github.com>
2022-03-14 09:45:54 +0100

    Add files via upload

--- IN1010/oblig_4 felles/src/Lenkeliste.java
+++ IN1010/oblig_4 felles/src/Lenkeliste.java
@@ -1,223 +1,122 @@
-/* ABSTRACT "MASTER"-KLASSE FOR ALLE KLASSENE I OBLIGEN
-IMPLEMENTASJON AV LINKEDLIST UTEN AA BRUKE JAVA.UTIL
-Implementerer interfacet Liste<T>
-Bruker hjelpeklassen Node for aa haandtere elementer
+import java.util.Iterator;
 
-int stoerrelse = antall elementer i listen
-Node start = foerste element i listen
-Node slutt = siste element i listen
 
-- metoden stoerrelse() returnerer variabelen stoerrelse
-- leggTil(T x) legger til et element sist i listen og oppdaterer var slutt
-- leggTilFoerst(T x) legger til et element foerst i listen og oppdaterer var start
-- leggTil(int pos, T x) legger til et element paa oppgitt indeks og forskyver resten
-- hent() returnerer det foerste elementet i listen uten aa endre det
-- fjern() sletter det foerste elementet i listen og returnere det 
-- toString() gaar gjennom listen og skriver ut informasjon om elementene */
-
-abstract class Lenkeliste <T> implements Liste <T> { 
+abstract class Lenkeliste <T> implements Liste<T> { 
     
-    /* GLOBALE VARIABLER FOR LENKELISTEN */
-    protected int stoerrelse;
-    protected Node start;
-    protected Node slutt;
-
-    /* HJELPEKLASSE NODE SOM HAANDTERER ELEMENTENE OG PEKERE */  
-    public class Node {
-
-        /* INSTANSVARIABLER FOR HVER NODE */
-        T data;
-        Node neste = null;
-        Node forrige = null;
-
-        /* KONSTRUKTOER FOR NODE */
-        Node(T data) {
-            this.data = data;
-        }
-    }
-
-    /* KONSTRUKTOER FOR LENKELISTEN */
-    public Lenkeliste() {
-        this.stoerrelse = 0; // default for stoerrelse
-        this.start = null; // default = tom
-        this.slutt = null; // default = tom
-    }
-
-    /* RETURNERER ANTALL ELEMENTER I LENKELISTEN */
-    public int stoerrelse() {
-        return stoerrelse;
-    }
-
-    /* SETTER INN NY NODE AV X SIST I LENKELISTEN */
-    public void leggTil(T x) {
-        // 1. lage den nye noden med data fra argument
-        Node ny_node = new Node(x);
-
-        // 3. sette neste for den nye noden til null, siden den skal vaere sist
-        ny_node.neste = null;
-
-        // 4. hvis den start noden allerede er null, sett inn noden her
-        if (start == null) {
-            ny_node.forrige = null; // bare 1 node, saa forrige er null
-            start = ny_node; // start node er naa den nye noden
-            stoerrelse++; // oeker stoerrelse med 1
-            return;
-        }
-
-        // 2. lagre midlertidig node for aa loekke gjennom listen 
-        Node gjeldende = start;
-
-        // 5. ellers maa vi gaa gjennom hele listen
-        // saa lenge nodens neste ikke er null er vi fortsatt midt i listen
-        // naar nodens neste == null har vi naadd slutt
-        while (gjeldende.neste != null) {
-            gjeldende = gjeldende.neste;
-        }
-       
-        // 6. ny node blir den "neste" av den slutt noden
-        gjeldende.neste = ny_node;  
-
-        // 7. ny node peker tilbake til den slutt noden
-        ny_node.forrige = gjeldende; 
-
-        // 8. den nye noden blir den siste noden
-        slutt = ny_node; 
-
-        // 9. oeker stoerrelse med 1
-        stoerrelse++; 
-    }
-
-    /* LEGGER TIL NY NODE AV X HELT FOERST I LISTEN */
-    public void leggTilFoerst(T x) {
-        // 1. lager ny node med dataen fra argumentet
-        Node ny_node = new Node(x);
-
-        // 2. skal vaere foerst i listen, saa neste maa vaere den gamle
-        // start. Nodens forrige skal vaere null
-        ny_node.neste = start;
-        ny_node.forrige = null;
-
-        // 3. hvis start finnes, setter starts forrige til den nye noden
-        if (start != null) {
-            start.forrige = ny_node;
-        }
-
-        // 4. setter den nye noden som ny start
-        start = ny_node;
-
-        // 5. oeker stoerrelse med 1
-        stoerrelse++;
-    }
-
-    /* LEGGER TIL NODE AV X PAA GITT INDEKS I LISTEN */
-    public void leggTil(int pos, T x) {
-        // 1. lage midlertidig node med ny data
-        Node ny_node = new Node(x);
-
-        // 2. sjekke om pos er gyldig
-        // lovlig pos: 0<=pos<=stoerrelse(), 
-        if (pos < 0 || pos > stoerrelse()) {
-            throw new UgyldigListeindeks(pos);
-
-        } else if (pos == 0) {
-            // 2.1 legger til foran hvis indeks er 0
-            leggTilFoerst(x);
-
-        } else if (pos == stoerrelse()) {
-            // 2.2 legger til sist hvis indeksen er lik stoerrelsen
-            leggTil(x); 
-
-        } else {
-            // 3. setter midlertidig node som den start
-            Node gjeldende = start;
-
-            // 3.1 gaar gjennom listen frem til riktig indeks
-            for (int i = 0; i < pos; i++) {
-                gjeldende = gjeldende.neste;
-            }
-
-            // 3.2 lagrer noden som var foer indeksplassen
-            Node forrige_node = gjeldende.forrige;
-
-            // 3.3 den forrige noden peker paa ny_node
-            forrige_node.neste = ny_node;
-
-            // 3.4 den nye noden peker tilbake paa forrige
-            ny_node.forrige = forrige_node;
-
-            // 3.5 den nye noden peker frem til indeksplassen
-            ny_node.neste = gjeldende;
-
-            // 3.6 noden paa indeksplassen peker tilbake paa den nye noden
-            gjeldende.forrige = ny_node;
-
-            // 3.7 oeker stoerrelsen med 1
-            stoerrelse++;
-        }
-    }
-
-    /* RETURNERER DEN FOERSTE NODEN I LENKELISTEN */
-    public T hent() {
-        // 1. throw exception hvis listen er tom (ingenting aa hente)
-        if (stoerrelse() == 0) {
-            throw new UgyldigListeindeks(0);
-
-        // 2. ellers returnerer den dataen til foerste element
-        } else {
-            return start.data;
-        }
-    }
-
-    /* FJERNER OG RETURNERER DEN FOERSTE NODEN I LENKELISTEN */
-    public T fjern() {
-        // 1. lagre noden midlertidig for aa kunne returnere den
-        Node skal_fjernes = start;
-
-        // 2. throw UgyldigListeindeks hvis listen er tom (ingenting aa fjerne)
-        if (stoerrelse() == 0) {
-            throw new UgyldigListeindeks(0);
-
-        // 3. ellers oppdater "start" og endre stoerrelse
-        } else {
-            // 3.1 ny start = den neste i listen
-            start = start.neste; 
-
-            // 3.2 hvis ny start finnes skal forrige settes til null
-            if (start != null) {
-                start.forrige = null; 
-            }
-
-            // 3.3 reduserer stoerrelse med 1
-            stoerrelse--;
-
-            // 3.4 returnerer dataen til noden som ble fjernet
-            return skal_fjernes.data;
-        }
-    }
-
-    /* LAGRER ALL DATAEN I LISTEN I EN STRENG */
-    @Override 
-    public String toString() {
-        // 1. definerer en streng for aa lagre hver linje i listen
-        String resultat = "####################\n";
+    
+        protected int antiBruk;
+        protected Node forste;
+        protected Node siste; 
+    
+        class Node{  //oppretter en Node-klasse 
+            T data;
+            Node neste = null;
+            Node forrige = null;
+            
         
-        // 2. setter midlertidig node som den foerste
-        Node gjeldende = start;
-
-        // 3. definerer en teller til loekken
-        int teller = 0;
-
-        // 4. loekke saa lenge det er flere elementer i listen
-        while (gjeldende != null) {
-            resultat = resultat.concat(teller + ": " + (String) gjeldende.data + "\n"); // oppdaterer resultat med dataen fra elementet
-            gjeldende = gjeldende.neste; // gaar videre til neste element
-            teller++; // oeker telleren med 1
+            Node(T data) { 
+              this.data = data; }
         }
+    
 
-        // 5. legger til en skillelinje til
-        resultat = resultat.concat("####################");
+     //indre klasse for iterator
+    class LenkelisteIterator implements Iterator<T>{
+    //Hvordan skal dette implementeres egentlig?
 
-        // 6. returnerer den ferdige Stringen
-        return resultat;
+
+    //Instansvariabler
+    protected Node p = forste;
+
+    //sjekk om element eksisterer
+    public boolean hasNext(){
+      if( p != null ){
+        return true;
+      }
+      else{
+        return false;
+      }
     }
+
+    //returner neste element
+    public T next(){
+
+      Node temp = p;
+
+      //flytter peker til neste node slik at hasNext() kan sjekke
+      p = p.neste;
+
+      //returner data fra elementet vi startet med i metoden
+      return temp.data;
+    }
+
+
+    public void remove(){}
+
+  }
+
+
+    public int storrelse(){
+        return antiBruk;
+    }   //returnere hvor mange elementer det er i listen.
+
+   
+
+    public void leggTil(T x)
+    {   Node nodeEn = new Node(x); //Oppretter først en ny node
+
+        //legger til første node dersom det ikke er noen i listen i starten
+        if(forste == null)
+        {  forste = nodeEn;
+        }
+    
+        else {  //legg node til i slutten av lenken
+
+            Node a = forste;  // lager Node a som utgangspunkt
+            while(a.neste != null){
+                a = a.neste;   // gå videre dersom "neste" peker på en node
+            }
+
+            a.neste = nodeEn;
+            } // lagrer noden i enden av lenken.
+
+        antiBruk++; //oppdatterer antallet når Node er lagt til
+        return;
+    }
+    
+
+    public T hent() throws UgyldigListeindeks{
+        if (forste == null) {throw new UgyldigListeindeks(-1); }
+        return forste.data;
+    } //returnere det 1. elementet i listen, uten at det fjernes fra listen.
+  
+
+   
+    public T fjern() throws UgyldigListeindeks{
+        if (antiBruk == 0) {throw new UgyldigListeindeks(0); }
+        Node a = forste;    //lagrer 1. node
+        forste = a.neste;
+        antiBruk--;        // fjerner en node fra total-antall
+        return a.data;    // returnerer den første noden
+    } 
+
+     //iterator objekt
+    public Iterator<T> iterator(){
+    LenkelisteIterator iterator = new LenkelisteIterator();
+    return iterator;
+    }
+
+
+    @Override
+    public String toString(){ //overskriver toString metode til Object, returnerer leselig strenge for lenkeliste
+        String nodeinfo = " ";
+        Node nodeEn = forste;
+        for (int i = 0; i<storrelse(); i++){
+            nodeinfo += nodeEn.data + "-->";
+            nodeEn = nodeEn.neste;
+        }
+        return nodeinfo; //Skriver ut lesbar string med nodeinfo
+    }
+        
 }
+
+
--- IN1010/oblig_4 felles/src/Pasient.java
+++ IN1010/oblig_4 felles/src/Pasient.java
@@ -1,3 +1,66 @@
-public class Pasient {
-    
+class Pasient {
+    private String navn;
+    private String fnr;
+    private int id;
+
+    static int antallID = 0;
+
+    Stabel<Resept> reseptliste = new Stabel<>();
+
+
+    public Pasient(String navn, String fnr)
+    {
+        this.navn = navn;
+        this.fnr = fnr;
+        this.id = antallID;
+        oppdaterIdTeller();
+    }
+
+
+    void oppdaterIdTeller()
+    {   antallID++;
+    }
+
+
+    Stabel<Resept> hentReseptliste()
+    {
+        return this.reseptliste;
+    }
+
+
+    void leggTilResept(Resept nyResept)
+    {
+        this.reseptliste.leggTil(nyResept);
+    }
+
+
+    public String hentNavn()
+    {
+        return this.navn;
+    }
+
+
+    public int hentId()
+    {
+        return this.id;
+    }
+
+
+    public String hentFnr()
+    {
+        return this.fnr;
+    }
+
+
+    //Metoder
+  
+    @Override
+    public String toString(){
+      return ("PASIENT\n" +
+              "ID: " + hentId() + ", Navn: " + hentNavn() +
+              "\nFodselsnr: " + hentFnr() +
+              "\nAntall resepter: " + hentReseptliste().storrelse() );
+    }
 }
+  
+   
