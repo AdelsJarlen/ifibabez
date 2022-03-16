bae4adc62a4bfc9cf12600e8f348a5af4149bb3a
jorgenosberg <jorgen.osberg@gmail.com>
2022-03-16 20:29:50 +0100

    Testet legesystem()

--- IN1010/oblig_4 felles/src/Main.java
+++ IN1010/oblig_4 felles/src/Main.java
@@ -1,33 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Pasient julie = new Pasient("Julie Haukaas", "010198");
-        Pasient synne = new Pasient("Synne Markmanrud", "260496");
-        Pasient jorgen = new Pasient("Jorgen Osberg", "060598");
 
-        // Lager tre legemidler (kopiert fra TestLegemiddel.java)
-        Vanlig kortison = new Vanlig("Kortison", 800.0, 179);
-        Narkotisk morfin = new Narkotisk("Morfin", 1050.0, 750, 5);
-        Vanedannende voltaren = new Vanedannende("Voltaren", 620.0, 225, 2);
+        /* TEST AV LEGESYSTEM */
 
-        // Lager et ekstra legemiddel som passer enda litt bedre til PResept
-        Vanlig pPiller = new Vanlig("P-piller", 450.0, 259);
+        Legesystem legesystem = new Legesystem();
 
-        // Lager en spesialistLege
-        Spesialist spesialistLege = new Spesialist("Dr. Andresen", "kontrollString");
-
-        // Lager 4 resepter med legemidlene og spesialisten
-        HvitResept hvitResept = new HvitResept(kortison, spesialistLege, julie, 10);
-        BlaaResept blaaResept = new BlaaResept(morfin, spesialistLege, synne, 1);
-        MilResept militaerResept = new MilResept(voltaren, spesialistLege, julie);
-        PResept pResept = new PResept(pPiller, spesialistLege, jorgen, 5);
-
-        synne.leggTilResept(blaaResept);
-        julie.leggTilResept(hvitResept);
-        julie.leggTilResept(militaerResept);
-        jorgen.leggTilResept(pResept);
-
-        // System.out.println(julie + "\n\n" + synne + "\n\n" + jorgen);
-
-        // System.out.println("Jorgen har resepten: " + jorgen.hentReseptliste().hent().hentLegemiddel().hentNavn());
+        legesystem.lesFraFil("IN1010/oblig_4 felles/legedata.txt");
     }
 }
