6b94778dc6830f88805398eb758a9cfab5409e12
jorgenosberg <jorgen.osberg@gmail.com>
2022-05-08 15:10:20 +0200

    Create 1060test.ino
    Ny fil for å teste de grunnleggende funksjonene enheten skal ha

---
+++ IN1060/1060test/1060test.ino
@@ -0,0 +1,18 @@
+const knapp1 = 3;
+const knapp2 = 5;
+const knapp3 = 7;
+
+const led1 = 2;
+const led2 = 4;
+const led3 = 6;
+
+void setup() {
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
