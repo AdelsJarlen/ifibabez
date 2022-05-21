f11665680242aaeec62bdf37b8b820bd4064727c
jorgenosberg <jorgen.osberg@gmail.com>
2022-05-21 14:41:46 +0200

    Testet buzzeren med Tone32

--- IN1060/Diverse testfiler/Tone32Test/Tone32Test.ino
+++ IN1060/Diverse testfiler/Tone32Test/Tone32Test.ino
@@ -1,32 +1,27 @@
-#include "pitches.h"
-// notes in the melody:
+#include <pitches.h>
+
+const int BUZZER_PIN = 11; // passiv buzzer
+
+// definerer Game Over-melodien fra Super Mario Bros. med varighet paa hver note
 int melody[] = {
 NOTE_C4, 0, NOTE_G3, 0, NOTE_E3, NOTE_A3, 
 NOTE_B3, NOTE_A3, NOTE_GS3, NOTE_AS3,
 NOTE_GS3, NOTE_G3, NOTE_F3, NOTE_G3
 };
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-
 int noteDurations[] = {
    4,6,4,6,4,5,5,5,5,5,5,8,8,2
 };
 
-void setup() {
-   
-   /*// itererer over hver note i melodien
-   for (int thisNote = 0; thisNote < 14; thisNote++) {
+
+void setup() {}
+
+void loop() {
+  for (int thisNote = 0; thisNote < 14; thisNote++) {
       // fjerdedel = 1000 / 4, aattendedel = 1000/8,
       // triol = 1000/5 o.l.
       int noteDuration = 1000/noteDurations[thisNote];
-      tone(11, melody[thisNote],noteDuration);
+      tone(BUZZER_PIN, melody[thisNote],noteDuration);
       // en liten pause mellom hver note
       delay(noteDuration +30);
-   }
-   */
-}
-
-void loop() {
-   tone(11, NOTE_C4, 500);
-   noTone(11);
-   delay(100);
+  }
 }
