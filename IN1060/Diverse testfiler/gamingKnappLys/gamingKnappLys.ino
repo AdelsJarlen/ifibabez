8c3bd9f429f2eaa2628102d25bcbed7c227235b3
jorgenosberg <jorgen.osberg@gmail.com>
2022-05-25 12:37:00 +0200

    Endringer i test-filen for å ha et enkelt, fungerende program til filmingen

--- IN1060/Diverse testfiler/gamingKnappLys/gamingKnappLys.ino
+++ IN1060/Diverse testfiler/gamingKnappLys/gamingKnappLys.ino
@@ -1,16 +1,17 @@
 #include <OneButton.h>
 #include <Adafruit_NeoPixel.h>
 
-#define BUTTON_1 10
-#define BUTTON_2 11
-#define BUTTON_3 12
+#define BUTTON_1 13
+#define BUTTON_2 12
+#define BUTTON_3 11
 
-#define LED_1    A2
-#define LED_2    A3
-#define LED_3    A4
+#define LED_1 A2
+#define LED_2 A1
+#define LED_3 A3
+
 #define LED_COUNT 1
 
-#define BUZZER A5
+#define BUZZER A4
 
 Adafruit_NeoPixel led1(LED_COUNT, LED_1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel led2(LED_COUNT, LED_2, NEO_GRB + NEO_KHZ800);
@@ -29,50 +30,78 @@ bool state2;
 bool state3;
 
 void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
   Serial.begin(115200);
   Serial.println("... starter serial");
-  
-  btn.attachClick(handleClick);
+
+  btn1.attachClick(startLED1);
+  btn2.attachClick(startLED2);
+  btn3.attachClick(startLED3);
   
   led1.begin();
-  led1.clear();
-  led1.setBrightness(150);
+  led1.setBrightness(255);
+  led1.setPixelColor(0, 0,0,0);
+  led1.show();
 
-  strip2.begin();
-  strip2.clear();
-  strip2.setBrightness(150);
+  led2.begin();
+  led2.setBrightness(255);
+  led2.setPixelColor(0, 0,0,0);
+  led2.show();
 
-  strip3.begin();
-  strip4.clear();
-  strip5.setBrightness(150);
-  state = true;
+  led3.begin();
+  led3.setBrightness(255);
+  led3.setPixelColor(0, 0,0,0);
+  led3.show();
+
+  state1 = false;
+  state2 = false;
+  state3 = false;
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  btn.tick();
+  btn1.tick();
+  btn2.tick();
+  btn3.tick();
   
 }
 
-static void handleClick() {
-  Serial.println("Clicked!");
-  
-  if (state == true) {
-      Serial.println("knapp, state er true");
-      strip.clear();
-      strip.setPixelColor(0, 255, 0, 0);
-      strip.setPixelColor(1, 0, 255, 0);
-      strip.setPixelColor(2, 0, 0, 255);
-      strip.show();
-      state = false;
-    } else if (state == false) {
-      Serial.println("knapp, state er false");
-      strip.clear();
-      strip.setPixelColor(0, 0, 255, 0);
-      strip.setPixelColor(1, 0, 0, 255);
-      strip.setPixelColor(2, 255, 0, 0);
-      strip.show();
-      state = true;
-    }
+static void startLED1() {
+  Serial.println("Button 1 clicked!");
+  if (state1 == false) {
+      state1 = true;
+      led1.setPixelColor(0, 255,255,255);
+      led1.show();
+   } else {
+      led1.setPixelColor(0, 0,0,0);
+      led1.show();
+      state1 = false;
+   }
+
+}
+
+static void startLED2() {
+  Serial.println("Button 2 clicked!");
+   if (state2 == false) {
+      state2 = true;
+      led2.setPixelColor(0, 255,255,255);
+      led2.show();
+   } else {
+      led2.setPixelColor(0, 0,0,0);
+      led2.show();
+      state2 = false;
+   }
+}
+
+static void startLED3() {
+  Serial.println("Button 3 clicked!");
+   if (state3 == false) {
+      state3 = true;
+      led3.setPixelColor(0, 255,255,255);
+      led3.show();
+   } else {
+      led3.setPixelColor(0, 0,0,0);
+      led3.show();
+      state3 = false;
+   }
 }
